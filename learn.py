e42a27887646c9da0f962040de5f3d8abc7dcb59
farhanz17 <mufhan623@gmail.com>
2021-10-19 21:55:30 +0700

    menambahkan learn.py

---
+++ learn.py
@@ -0,0 +1,19 @@
+##Input Data##
+name = "Muhammad Farhan."
+nickname = "Farhan."
+nim ="312110128."
+born = "Jakarta"
+old = "18"
+address = "Perum.Kota Serang Baru.Blok C 70 No 9."
+numb = "081218904517 (WhatsApp Only)"
+
+##Output Data##
+print ("Assalammu'alaikum..")
+
+print ()
+
+print ("Let me introduce my self. My name is " +name,"But you can call me " +nickname,"My NIM is " +nim,"I was born in " + born,"and I am "+old,"years old.","I am very glad if you want to invite my house in " +address,"So don't forget to call me before with the number " +numb)
+
+print ()
+print ("Thank you.")
+##Output done##
